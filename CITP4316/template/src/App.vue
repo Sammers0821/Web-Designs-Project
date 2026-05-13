be15c4dd3dd6e924c547d23092b665712400badc
Sammers0821 <sammygrace0821@gmail.com>
2026-05-12 20:10:35 -0500

    Initial API layout

---
+++ CITP4316/template/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import NavMenu from '@/components/NavMenu.vue'
+</script>
+
+<template>
+  <div class="flex h-screen flex-col">
+    <NavMenu />
+    <RouterView />
+  </div>
+</template>
+
