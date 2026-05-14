188660041e9e58ce3f26a1a388cf698ba4418eb0
Sammers0821 <sammygrace0821@gmail.com>
2026-05-13 19:38:39 -0500

    Added idParam validation

---
+++ CITP4316/my-projects-rest-api/src/routes/projects.js
@@ -0,0 +1,26 @@
+import { Hono } from 'hono'
+import { listProjects, getProjectById } from '../data/store.js'
+import { ApiError } from '../utils/errors.js'
+import { sendCollection, sendResource } from '../utils/response.js'
+import { parseIdParam } from '../utils/validation.js'
+
+
+const projects = new Hono()
+
+projects.get('/', (c) => {
+    const data = listProjects()
+    return sendCollection(c, data)
+})
+
+projects.get('/:id', (c) => {
+    const id = parseIdParam(+c.req.param('id'))
+
+    const project = getProjectById(id)
+
+    if (!project) {
+        throw new ApiError(404, 'NOT_FOUND', 'Project not found')
+    }
+
+    return sendResource(c, project)
+})
+export default projects
