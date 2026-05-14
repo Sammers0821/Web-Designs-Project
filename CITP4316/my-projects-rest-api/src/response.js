321bfaf703fe3ee46c01031d2d14226a1920059a
Sammers0821 <sammygrace0821@gmail.com>
2026-05-14 00:59:57 -0500

    added taks and logic

--- CITP4316/my-projects-rest-api/src/response.js
+++ CITP4316/my-projects-rest-api/src/response.js
@@ -1,26 +1,26 @@
 export function getTraceId(c) {
-    return c.get('traceId') || crypto.randomUUID()
+  return c.get('traceId') || crypto.randomUUID()
 }
 
 export function sendResource(c, data, status = 200) {
-    return c.json({ data }, status)
+  return c.json({ data }, status)
 }
 
 export function sendCollection(c, data, status = 200) {
-    return c.json({ data, meta: {count: data.lenght} }, status)
+  return c.json({ data, meta: { count: data.length } }, status)
 }
 
 export function buildErrorPayload(c, code, message, details = []) {
-    return { 
-        error: {
-            code, 
-            message, 
-            details,
-            trace_id: getTraceId(c),
-        },
-    }
+  return {
+    error: {
+      code,
+      message,
+      details,
+      trace_id: getTraceId(c),
+    },
+  }
 }
 
 export function sendError(c, status, code, message, details = []) {
-    return c.json(buildErrorPayload(c, code, message,details), status)
+  return c.json(buildErrorPayload(c, code, message, details), status)
 }
